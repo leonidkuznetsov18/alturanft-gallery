47a3a76c517fcba9c528b6fa9853324a8b859f59
Leonid Kuznetsov <gitmrche@gmail.com>
2023-03-08 20:32:12 +0200

    fix: update logic

--- hooks/useNFTs.tsx
+++ hooks/useNFTs.tsx
@@ -52,7 +52,7 @@ export const useNFTs = () => {
       if (res.pageKey) {
         setPageKey(res.pageKey);
       } else {
-        setPageKey(pageKey);
+        setPageKey(undefined);
       }
       return res;
     } catch (error) {
